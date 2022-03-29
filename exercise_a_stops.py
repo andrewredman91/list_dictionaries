6bba55e7ef7be12a4393ffeb7c93b20a67b0ae85
andrewredman91 <andrewredman91@gmail.com>
2022-03-29 16:04:46 +0100

    q4 complete

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -3,9 +3,11 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 #1. Add "Edinburgh Waverley" to the end of the list
 stops.append("Edinburgh Waverley")
 #2. Add "Glasgow Queen St" to the start of the list
-stops.append(insert("Glasgow Queen St"))
+stops.append(insert(0,"Glasgow Queen St"))
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.append(insert(4, "Polmont"))
 #4. Print out the index position of "Linlithgow"
+print(index(5))
 #5. Remove "Livingston" from the list using its name
 #6. Delete "Cumbernauld" from the list by index
 #7. Print the number of stops there are in the list
